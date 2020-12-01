fd862f5456493244f9dc7f0c7a0df409e2a52cd1
Fernando Barbosa <f.barbosa@prestserv.guide.com.br>
2020-12-01 20:15:16 -0300

    Exercicio de Fixação de estruturas condicionais 01

---
+++ Iniciando-com-dotnet/FixationExerciseConditionalStructure1/FixationExerciseConditionalStructure1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace FixationExerciseConditionalStructure1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                Fazer um programa para ler um número inteiro, e depois dizer se este número é negativo ou não.
+             */
+
+            Console.WriteLine("Digite um valor inteiro.");
+            int value = int.Parse(Console.ReadLine());
+
+            if (value < 0)
+            {
+                Console.WriteLine("O valor digitado é negativo");
+            }
+            else
+            {
+                Console.WriteLine("O valor digitado é positivo");
+            }
+        }
+    }
+}
