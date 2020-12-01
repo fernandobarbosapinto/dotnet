8b42232e398023fd3b6df4be9078cb3882dff9c0
Fernando Barbosa <f.barbosa@prestserv.guide.com.br>
2020-12-01 11:38:37 -0300

    Diferenças entre Placeholders, Concatenação e Interpolação

--- CourseTypeBasic/CourseTypeBasic/Program.cs
+++ CourseTypeBasic/CourseTypeBasic/Program.cs
@@ -7,22 +7,13 @@ namespace CourseTypeBasic
     {
         static void Main(string[] args)
         {
-            char genero = 'F';
             int idade = 32;
             double saldo = 10.35784;
             String nome = "Maria";
 
-            Console.Write("Bom dia!"); //Console.Write não quebra linha
-            Console.WriteLine("Boa tarde!"); //Console.WriteLine faz quebra de linha
-            Console.WriteLine("Boa noite!");
-            Console.WriteLine("---------------------------");
-            Console.WriteLine(genero);
-            Console.WriteLine(idade);
-            Console.WriteLine(saldo);
-            Console.WriteLine(nome);
-            Console.WriteLine(saldo.ToString("F2")); //Imprime duas casa decimais
-            Console.WriteLine(saldo.ToString("F4")); //Imprime quatro casas decimais
-            Console.WriteLine(saldo.ToString("F4", CultureInfo.InvariantCulture)); //Utilizando Format Provider para exibir o ponto de separação na casa decimal
+            Console.WriteLine("{0} tem {1} anos e tem saldo igual a {2:F2} reais", nome, idade, saldo); //Placeholders
+            Console.WriteLine($"{nome} tem {idade} anos e tem saldo igual a {saldo:F2} reais"); //Interpolação
+            Console.WriteLine(nome + " tem " + idade + " anos e tem saldo igual a " + saldo.ToString("F2", CultureInfo.InvariantCulture) + " reais"); //Concatenação
         }
     }
 }
