f188e4071f603c6092561de6dd38697c3faaeb10
Fernando Barbosa <f.barbosa@prestserv.guide.com.br>
2020-12-01 14:43:48 -0300

    Exercicio de Fixação 01

---
+++ FixationExercise1/FixationExercise1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace FixationExercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A, B, soma;
+            Console.WriteLine("Digite o primeiro valor: ");
+            A = int.Parse(Console.ReadLine());
+            
+            Console.WriteLine("Digite o segundo valor: ");
+            B = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("O valor da soma é!");
+            soma = A + B;
+
+            Console.WriteLine("Soma = " + soma);
+        }
+    }
+}
