edede30ed6d5b262c56086ea728a01de0561425d
Fernando Barbosa <f.barbosa@prestserv.guide.com.br>
2020-12-01 12:45:51 -0300

    Conhecendo o comando Console.ReadLine

--- CourseTypeBasic/CourseTypeBasic/Program.cs
+++ CourseTypeBasic/CourseTypeBasic/Program.cs
@@ -7,12 +7,22 @@ namespace CourseTypeBasic
     {
         static void Main(string[] args)
         {
-            int a = 10;
-            a++;
+            string frase = Console.ReadLine();
+            string x = Console.ReadLine();
+            string y = Console.ReadLine();
+            string z = Console.ReadLine();
+            string[] v = Console.ReadLine().Split(' ');
+            string a = v[0];
+            string b = v[1];
+            string c = v[2];
+            Console.WriteLine("Você digitou: ");
+            Console.WriteLine(frase);
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(z);
             Console.WriteLine(a);
-
-            a--;
-            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
         }
     }
 }
