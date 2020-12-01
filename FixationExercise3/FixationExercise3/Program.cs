ba7b360206e6dc11e817a7585a3962e81c44f761
Fernando Barbosa <f.barbosa@prestserv.guide.com.br>
2020-12-01 15:47:16 -0300

    Exercicio de Fixação 03

---
+++ FixationExercise3/FixationExercise3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace FixationExercise3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                Fazer um programa para ler quatro valores inteiros A, B, C e D. A seguir, calcule e mostre a diferença do produto
+                de A e B pelo produto de C e D segundo a fórmula: DIFERENCA = (A * B - C * D).
+             */
+
+            int A, B, C, D, dif;
+
+            Console.WriteLine("Digite o primeiro valor:");
+            A = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo valor:");
+            B = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o terceiro valor:");
+            C = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o quarto valor:");
+            D = int.Parse(Console.ReadLine());
+
+            dif = A * B - C * D;
+
+            Console.WriteLine("DIFERENCA DOS VALORES É = " + dif);
+        }
+    }
+}
