b5b51d996d6a1fae6589404b1b67f291b3c00095
Fernando Barbosa <f.barbosa@prestserv.guide.com.br>
2020-12-01 15:24:29 -0300

    Exercicio de Fixação 02

---
+++ FixationExercise2/FixationExercise2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace FixationExercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                Faça um programa para ler o valor do raio de um círculo, e depois mostrar o valor da área deste círculo com quatro
+                casas decimais conforme exemplos.
+             */
+            double R, A, pi = 3.14159;
+
+            Console.WriteLine("Digite o valor do raio");
+            R = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            A = pi * R * R;
+
+            Console.WriteLine("Valor da Área: " + A.ToString("F4", CultureInfo.InvariantCulture));
+        }
+    }
+}
