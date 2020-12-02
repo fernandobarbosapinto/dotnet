888e185212d8021b4c766985705e6cae0a872dd4
Fernando Barbosa <f.barbosa@prestserv.guide.com.br>
2020-12-02 16:17:28 -0300

    Exercicio de Fixação utilizando For 06

---
+++ Iniciando-com-dotnet/FixationExerciseFor6/FixationExerciseFor6/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace FixationExerciseFor6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                Ler um número inteiro N e calcular todos os seus divisores.
+             */
+
+            Console.WriteLine("Digite um valor inteiro.");
+            int N = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= N; i++)
+            {
+                if (N % i == 0)
+                {
+                    Console.WriteLine($"Os valores divisiveis por {N} são ===> {i}");
+                }
+            }
+        }
+    }
+}
