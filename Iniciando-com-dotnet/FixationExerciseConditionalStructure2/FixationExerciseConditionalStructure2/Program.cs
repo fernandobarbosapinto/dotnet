d308221d1899af0315c0639a3de1aa1aaa47a41d
Fernando Barbosa <f.barbosa@prestserv.guide.com.br>
2020-12-01 21:00:54 -0300

    Exercicio de Fixação de estruturas condicionais 02

---
+++ Iniciando-com-dotnet/FixationExerciseConditionalStructure2/FixationExerciseConditionalStructure2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace FixationExerciseConditionalStructure2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                Leia 2 valores inteiros (A e B). Após, o programa deve mostrar uma mensagem "Sao Multiplos" ou "Nao sao
+                Multiplos", indicando se os valores lidos são múltiplos entre si. Atenção: os números devem poder ser digitados em
+                ordem crescente ou decrescente
+             */
+
+            Console.WriteLine("Digite dois valores separados usando o espaço.");
+            string[] valores = Console.ReadLine().Split(' ');
+            int A = int.Parse(valores[0]);
+            int B = int.Parse(valores[1]);
+
+            if (A % B == 0 || B % A == 0)
+            {
+                Console.WriteLine("Sao Multiplos");
+            }
+            else
+            {
+                Console.WriteLine("Nao sao Multiplos");
+            }
+        }
+    }
+}
