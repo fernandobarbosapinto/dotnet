eeed9cf6c20eda20e351e44d1c093df353682a64
Fernando Barbosa <f.barbosa@prestserv.guide.com.br>
2020-12-01 21:09:02 -0300

    Exercicio de Fixação de estruturas condicionais 03

---
+++ Iniciando-com-dotnet/FixationExerciseConditionalStructure3/FixationExerciseConditionalStructure3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace FixationExerciseConditionalStructure3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite a hora que o jogo começou e a hora que o jogo termino. Para separar de um espaço entre as horas");
+            string[] valores = Console.ReadLine().Split(' ');
+            int horaInicial = int.Parse(valores[0]);
+            int horaFinal = int.Parse(valores[1]);
+
+            int duracao;
+            if (horaInicial < horaFinal)
+            {
+                duracao = horaFinal - horaInicial;
+            }
+            else
+            {
+                duracao = 24 - horaInicial + horaFinal;
+            }
+
+            Console.WriteLine($"O JOGO DUROU {duracao} HORA(S)");
+        }
+    }
+}
