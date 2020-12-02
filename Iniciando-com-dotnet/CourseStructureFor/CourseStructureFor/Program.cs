b138e48b50d43cd411d24b6b3a7891b35ba8f71c
Fernando Barbosa <f.barbosa@prestserv.guide.com.br>
2020-12-02 14:28:26 -0300

    Utilizando a estrutura do For

---
+++ Iniciando-com-dotnet/CourseStructureFor/CourseStructureFor/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CourseStructureFor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Quantos números inteiros você vai digitar?");
+            int N = int.Parse(Console.ReadLine());
+
+            int soma = 0;
+
+            for (int i = 1; i <= N; i++)
+            {
+                Console.WriteLine($"Valor {i}: ");
+                int valor = int.Parse(Console.ReadLine());
+                soma += valor;
+            }
+
+            Console.WriteLine($"A soma dos valores é {soma}.");
+        }
+    }
+}
