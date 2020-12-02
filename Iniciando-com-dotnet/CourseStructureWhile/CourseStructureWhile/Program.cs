a7f90f1328057d6b8d373b4c02d7c2c1509c356b
Fernando Barbosa <f.barbosa@prestserv.guide.com.br>
2020-12-02 12:22:25 -0300

    Utilizando While

---
+++ Iniciando-com-dotnet/CourseStructureWhile/CourseStructureWhile/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+namespace CourseStructureWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um número:");
+            double x = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            while (x > 0.0)
+            {
+                double raiz = Math.Sqrt(x);
+                Console.WriteLine($"A raiz quadrada do número digitado é {raiz.ToString("F3", CultureInfo.InvariantCulture)}");
+                
+                Console.WriteLine("Digite outro número: ");
+                x = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+
+            Console.WriteLine("Número negativo!!!");
+        }
+    }
+}
