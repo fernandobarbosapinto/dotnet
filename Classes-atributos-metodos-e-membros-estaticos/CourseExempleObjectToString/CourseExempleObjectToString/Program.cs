c81b81444998206fed38ad6c7d217e39a899fbf4
Fernando Barbosa <f.barbosa@prestserv.guide.com.br>
2020-12-03 12:13:02 -0300

    Aprendendo a utilizar métodos dentro do objeto - GetType, Equals, GetHashCode e ToString

---
+++ Classes-atributos-metodos-e-membros-estaticos/CourseExempleObjectToString/CourseExempleObjectToString/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Globalization;
+namespace CourseExempleObjectToString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto p = new Produto();
+
+            Console.WriteLine("Cadastrar produto:");
+            Console.WriteLine("--------------------------------------------------------------------");
+            Console.Write("Nome: ");
+            p.Nome = Console.ReadLine();
+            Console.Write("Preço: ");
+            p.Preco = double.Parse(Console.ReadLine());
+            Console.Write("Quantidade no estoque: ");
+            p.Quantidade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.WriteLine($"Dados do produto: {p}");
+
+            Console.WriteLine("--------------------------------------------------------------------");
+            Console.WriteLine();
+            Console.WriteLine("Digite a quantidade de produtos a serem adicionados ao estoque: ");
+            int quantidade = int.Parse(Console.ReadLine());
+            p.AdicionarProdutos(quantidade);
+
+            Console.WriteLine("--------------------------------------------------------------------");
+            Console.WriteLine();
+            Console.WriteLine($"Atualização nos dados! Foi adicionado mais {quantidade} produtos: {p}");
+
+            Console.WriteLine("--------------------------------------------------------------------");
+            Console.WriteLine();
+            Console.WriteLine("Digite a quantidade de produtos a serem removidos do estoque: ");
+            quantidade = int.Parse(Console.ReadLine());
+            p.RemoverProdutos(quantidade);
+
+            Console.WriteLine("--------------------------------------------------------------------");
+            Console.WriteLine();
+            Console.WriteLine($"Atualização nos dados! Foi removido mais {quantidade} produtos: {p}");
+        }
+    }
+}
