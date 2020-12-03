0be737f2dc9d51da6fb25a9698289e9c8adf2614
Fernando Barbosa <f.barbosa@prestserv.guide.com.br>
2020-12-03 10:31:15 -0300

    Exercicio de Fixação utilizando classes 02

---
+++ Classes-atributos-metodos-e-membros-estaticos/FixationExerciseClasses2/FixationExerciseClasses2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Globalization;
+
+namespace FixationExerciseClasses2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario f1 = new Funcionario();
+            Funcionario f2 = new Funcionario();
+            
+            Console.WriteLine("Insira os dados do primeiro funcionário:");
+            Console.WriteLine("-----------------------------------------------------");
+
+            Console.Write("Nome: ");
+            f1.Nome = Console.ReadLine();
+            Console.Write("Salário: ");
+            f1.Salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine();
+            Console.WriteLine("Insira os dados do segundo funcionário:");
+            Console.WriteLine("-----------------------------------------------------");
+
+            Console.Write("Nome: ");
+            f2.Nome = Console.ReadLine();
+            Console.Write("Salário: ");
+            f2.Salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double media = (f1.Salario + f2.Salario) / 2.0;
+
+            Console.WriteLine();
+
+            Console.WriteLine($"O salário médio dos trabalhadores é: {media.ToString("F2", CultureInfo.InvariantCulture)}");
+
+        }
+
+    }
+}
