1fe78aa2656581c8612a0eff32860cefc6c335d2
Fernando Barbosa <f.barbosa@prestserv.guide.com.br>
2020-12-03 10:03:20 -0300

    Trabalhando com Classes

---
+++ Classes-atributos-metodos-e-membros-estaticos/CourseExempleNotObject/CourseExempleNotObject/Triangulo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CourseExempleNotObject
+{
+    class Triangulo
+    {
+        public double A;
+        public double B;
+        public double C;
+    }
+}
