b8730a431e8963093f854f05c10f75121ce49578
Fernando Barbosa <f.barbosa@prestserv.guide.com.br>
2020-12-03 10:19:33 -0300

    Exercicio de Fixação utilizando classes 01

---
+++ Classes-atributos-metodos-e-membros-estaticos/FixationExerciseClasses1/FixationExerciseClasses1/Pessoa.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FixationExerciseClasses1
+{
+    class Pessoa
+    {
+        public string nome;
+        public int idade;
+    }
+}
---
+++ Classes-atributos-metodos-e-membros-estaticos/FixationExerciseClasses1/FixationExerciseClasses1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace FixationExerciseClasses1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa p1 = new Pessoa();
+            Pessoa p2 = new Pessoa();
+
+            Console.WriteLine("Insira os dados da primeira pessoa:");
+            Console.WriteLine("-----------------------------------------------------------------");
+
+            Console.Write("Nome: ");
+            p1.nome = Console.ReadLine();
+            Console.Write("Idade: ");
+            p1.idade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.WriteLine("Insira os dados da segunda pessoa:");
+            Console.WriteLine("-----------------------------------------------------------------");
+
+            Console.Write("Nome: ");
+            p2.nome = Console.ReadLine();
+            Console.Write("Idade: ");
+            p2.idade = int.Parse(Console.ReadLine());
+
+            if (p1.idade > p2.idade)
+            {
+                Console.WriteLine($"A pessoa mais velha é {p1.nome}");
+            }
+            else
+            {
+                Console.WriteLine($"A pessoa mais velha é {p2.nome}");
+            }
+        }
+    }
+}
