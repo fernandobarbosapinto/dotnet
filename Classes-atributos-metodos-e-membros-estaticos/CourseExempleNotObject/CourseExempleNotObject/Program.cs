b1933556adc82f4a92ebd777a74bbb93a570f80b
Fernando Barbosa <f.barbosa@prestserv.guide.com.br>
2020-12-03 09:53:57 -0300

    Inserindo comentário da atividade

--- Classes-atributos-metodos-e-membros-estaticos/CourseExempleNotObject/CourseExempleNotObject/Program.cs
+++ Classes-atributos-metodos-e-membros-estaticos/CourseExempleNotObject/CourseExempleNotObject/Program.cs
@@ -7,6 +7,14 @@ namespace CourseExempleNotObject
     {
         static void Main(string[] args)
         {
+            /*
+                Fazer um programa para ler as medidas dos lados de dois triângulos X e Y (suponha medidas
+                válidas). Em seguida, mostrar o valor das áreas dos dois triângulos e dizer qual dos dois triângulos
+                possui a maior área.
+                A fórmula para calcular a área de um triângulo a partir das medidas de seus lados a, b e c é a
+                seguinte (fórmula de Heron):
+             */
+
             double xA, xB, xC, yA, yB, yC;
             Console.WriteLine("Entre com as medidas do triângulo X:");
             
