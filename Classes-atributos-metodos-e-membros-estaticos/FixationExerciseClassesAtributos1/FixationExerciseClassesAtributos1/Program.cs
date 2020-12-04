bf2c2f703336bb3304bb2e3df0fa42be750cb855
Fernando Barbosa <f.barbosa@prestserv.guide.com.br>
2020-12-04 10:52:37 -0300

    Exercicio de Fixação utilizando classes e atributos 01

---
+++ Classes-atributos-metodos-e-membros-estaticos/FixationExerciseClassesAtributos1/FixationExerciseClassesAtributos1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace FixationExerciseClassesAtributos1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo retangulo = new Retangulo();
+
+            Console.WriteLine("Digite a largura e a altura do retângulo: ");
+            retangulo.Largura = double.Parse(Console.ReadLine());
+            retangulo.Altura = double.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.WriteLine("--------------------------------------------------------");
+
+            Console.WriteLine($"Valor total da Àrea do retângulo: {retangulo.Area().ToString("F2", CultureInfo.InvariantCulture)}");
+            Console.WriteLine($"Valor total do Perimetro: {retangulo.Perimetro().ToString("F2", CultureInfo.InvariantCulture)}");
+            Console.WriteLine($"Valor total da Diagonal: {retangulo.Diagonal().ToString("F2", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
