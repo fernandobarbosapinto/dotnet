81c28c0d726cc4e3d0c0d6c3f7a0f4fa91ed9b84
Fernando Barbosa <f.barbosa@prestserv.guide.com.br>
2020-12-04 12:11:38 -0300

    Exercicio de Fixação utilizando classes e atributos 03

---
+++ Classes-atributos-metodos-e-membros-estaticos/FixationExerciseClassesAtributos3/FixationExerciseClassesAtributos3/Aluno.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FixationExerciseClassesAtributos3
+{
+    class Aluno
+    {
+        public string Nome;
+        public double Nota1, Nota2, Nota3, Nota4;
+
+        public double NotaFinal()
+        {
+            return (Nota1 + Nota2 + Nota3 + Nota4) / 4;
+        }
+
+        public bool Aprovado()
+        {
+            if (NotaFinal() >= 60.0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public double NotaRestante()
+        {
+            if (Aprovado())
+            {
+                return 0.0;
+            }
+            else
+            {
+                return 60.0 - NotaFinal();
+            }
+        }
+    }
+}
---
+++ Classes-atributos-metodos-e-membros-estaticos/FixationExerciseClassesAtributos3/FixationExerciseClassesAtributos3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+
+namespace FixationExerciseClassesAtributos3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno aluno = new Aluno();
+
+            Console.Write("Digite o nome do aluno: ");
+            aluno.Nome = Console.ReadLine();
+
+            Console.WriteLine();
+            Console.WriteLine("-------------------------------------------");
+            Console.WriteLine("Digite as 4 notas finais do Aluno:");
+            aluno.Nota1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            aluno.Nota2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            aluno.Nota3 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            aluno.Nota4 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine($"Nota Final= {aluno.NotaFinal().ToString("F2", CultureInfo.InvariantCulture)}");
+
+            if (aluno.Aprovado())
+            {
+                Console.WriteLine("Parabêns você foi Aprovado!");
+            }
+            else
+            {
+                Console.WriteLine($"Infelizmente você foi reprovado. Faltaram {aluno.NotaRestante().ToString("F2", CultureInfo.InvariantCulture)} pontos");
+            }
+
+        }
+    }
+}
