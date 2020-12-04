1d9ec5cf832fa75343ce13320a041e49359762a1
Fernando Barbosa <f.barbosa@prestserv.guide.com.br>
2020-12-04 11:24:50 -0300

    Exercicio de Fixação utilizando classes e atributos 02

---
+++ Classes-atributos-metodos-e-membros-estaticos/FixationExerciseClassesAtributos2/FixationExerciseClassesAtributos2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+namespace FixationExerciseClassesAtributos2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario funcionario = new Funcionario();
+
+            Console.Write("Nome: ");
+            funcionario.Nome = Console.ReadLine();
+            Console.Write("Salário Bruto: ");
+            funcionario.SalarioBruto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Imposto: ");
+            funcionario.Imposto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine();
+            Console.WriteLine("---------------------------------------------------------------------------------");
+            Console.WriteLine("Funcionário: " + funcionario);
+            Console.WriteLine();
+            Console.Write("Deseja aumentar o salário em qual porcentagem? ");
+            double porcent = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            funcionario.AumentarSalario(porcent);
+            
+            Console.WriteLine();
+            Console.WriteLine("---------------------------------------------------------------------------------");
+            Console.WriteLine("Dados atualizados: " + funcionario);
+        }
+    }
+}
