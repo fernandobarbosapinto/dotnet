c658915be4e7ab6a181316644080f0624be6322e
Fernando Barbosa <f.barbosa@prestserv.guide.com.br>
2020-12-04 15:29:38 -0300

    Exercicio de Fixação utilizando encapsulamento 01

---
+++ Construtores-this-sobrecarga-encapsulamento/FixationExerciseEncapsulamento/FixationExerciseEncapsulamento/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Globalization;
+
+namespace FixationExerciseEncapsulamento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaBancaria conta;
+
+            Console.Write("Digite o número da conta a ser cadastrada: ");
+            int numero = int.Parse(Console.ReadLine());
+            Console.Write("Digite o nome do títular da conta: ");
+            string titular = Console.ReadLine();
+            Console.Write("O cliente fez um depósito inicial? (s/n)");
+            char resp = char.Parse(Console.ReadLine());
+
+            if (resp == 's' || resp == 'S')
+            {
+                Console.Write("Digite o valor a ser depositado: ");
+                double depositoInicial = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                conta = new ContaBancaria(numero, titular, depositoInicial);
+            }
+            else
+            {
+                conta = new ContaBancaria(numero, titular);
+            }
+
+            Console.WriteLine("---------------------------------------------------------------------------");
+            Console.WriteLine();
+            Console.WriteLine("Dados da conta cadastrada.");
+            Console.WriteLine(conta);
+
+            Console.WriteLine("---------------------------------------------------------------------------");
+            Console.WriteLine();
+            Console.Write("Entre um valor para depósito: ");
+            double quantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta.Deposito(quantia);
+            Console.WriteLine("Dados da conta atualizados:");
+            Console.WriteLine(conta);
+
+            Console.WriteLine("---------------------------------------------------------------------------");
+            Console.WriteLine();
+            Console.Write("Entre um valor para saque: ");
+            quantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta.Saque(quantia);
+            Console.WriteLine("Dados da conta atualizados:");
+            Console.WriteLine(conta);
+        }
+    }
+}
