90690d6ba4968ed40969b6ad00cef889e5b92813
Fernando Barbosa <f.barbosa@prestserv.guide.com.br>
2020-12-04 12:59:10 -0300

    Aprendendo a utilizar membros estáticos segundo exemplo

--- Classes-atributos-metodos-e-membros-estaticos/CourseExempleStaticMembers/CourseExempleStaticMembers/Program.cs
+++ Classes-atributos-metodos-e-membros-estaticos/CourseExempleStaticMembers/CourseExempleStaticMembers/Program.cs
@@ -12,29 +12,20 @@ namespace CourseExempleStaticMembers
                 casas decimais.
         */
 
-        static double Pi = 3.14;
         static void Main(string[] args)
         {
+            Calculadora calculadora = new Calculadora();
+
             Console.WriteLine("Digite o valor do raio: ");
             double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-            double circunferencia = Circunferencia(raio);
-            double volume = Volume(raio);
+            double circunferencia = calculadora.Circunferencia(raio);
+            double volume = calculadora.Volume(raio);
 
             Console.WriteLine($"Valor da Circunferência: {circunferencia.ToString("F2", CultureInfo.InvariantCulture)}");
             Console.WriteLine($"Valor do Volume: {volume.ToString("F2", CultureInfo.InvariantCulture)}");
-            Console.WriteLine($"Valor de Pi: {Pi.ToString("F2", CultureInfo.InvariantCulture)}");
+            Console.WriteLine($"Valor de Pi: {calculadora.Pi.ToString("F2", CultureInfo.InvariantCulture)}");
 
-        }
-
-        static double Circunferencia(double raio)
-        {
-            return 2.0 * Pi * raio;
-        }
-
-        static double Volume(double raio)
-        {
-            return 4.0 / 3.0 * Pi * raio * raio * raio;
         }
     }
 }
