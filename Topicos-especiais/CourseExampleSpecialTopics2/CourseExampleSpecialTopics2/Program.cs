703cfb5155ba5f4ee70fc80a93291639f46e4dfa
Fernando Barbosa <f.barbosa@prestserv.guide.com.br>
2020-12-15 10:19:46 -0300

    Entendendo o funcionamento da expressão ternaria

---
+++ Topicos-especiais/CourseExampleSpecialTopics2/CourseExampleSpecialTopics2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace CourseExampleSpecialTopics2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Utilizando expressão ternária*/
+            
+            double preco = 34.5;
+            double desconto;
+
+            /*utilizando if else de forma comum*/
+            if (preco < 20.0)
+            {
+                desconto = preco * 0.1;
+                Console.WriteLine(desconto);
+            }
+            else
+            {
+                desconto = preco * 0.05;
+                Console.WriteLine(desconto);
+            }
+
+            /*utilizando expressão ternária*/
+            double preco2 = 34.5;
+            double desconto2 = (preco2 < 20.0) ? preco2 * 0.1 : preco2 * 0.05;
+            Console.WriteLine(desconto2);
+        }
+    }
+}
