0f4aaa369217cb27c33bb731bfe37ba8567db6f9
Fernando Barbosa <f.barbosa@prestserv.guide.com.br>
2021-02-17 13:20:55 -0300

    Aprendendo a utilizar enumeração

---
+++ Enumeracoes-e-composicao/enum1/enum1/Entities/Order.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using enum1.Entities.Enums;
+using System.Text;
+
+namespace enum1.Entities
+{
+    class Order
+    {
+        public int Id { get; set; }
+        public DateTime Moment { get; set; }
+        public OrderStatus Status { get; set; }
+
+        public override string ToString()
+        {
+            return Id
+                + ", "
+                + Moment
+                + ", "
+                + Status;
+        }
+    }
+}
---
+++ Enumeracoes-e-composicao/enum1/enum1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using enum1.Entities;
+using enum1.Entities.Enums;
+
+namespace enum1
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            Order order = new Order
+            {
+                Id = 1800,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+            };
+
+            Console.WriteLine(order);
+
+            //fazendo a conversão de um valor enumerado para string
+
+            string txt = OrderStatus.PendingPayment.ToString();
+
+            Console.WriteLine(txt);
+
+            //fazendo a conversão de uma string para um valor enumerado
+
+            OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+
+            Console.WriteLine(os);
+        }
+    }
+}
