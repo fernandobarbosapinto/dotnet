691a0bf9d927cd3e4110b89dd15a32bcdf7c1726
Fernando Barbosa <f.barbosa@prestserv.guide.com.br>
2021-03-09 08:03:14 -0300

    Implementando a logica de xequemate

--- xadrez_console/xadrez_console/xadrez/PartidaDeXadrez.cs
+++ xadrez_console/xadrez_console/xadrez/PartidaDeXadrez.cs
@@ -70,8 +70,15 @@ namespace xadrez
                 xeque = false;
             }
 
-            turno++;
-            mudarJogador();
+            if (testeXequemate(adversaria(jogadorAtual)))
+            {
+                terminada = true;
+            }
+            else
+            {
+                turno++;
+                mudarJogador();
+            }
         }
 
         public void validarPosicaoDeOrigem(Posicao pos)
@@ -179,6 +186,38 @@ namespace xadrez
                 }
             }
             return false;
+        }
+
+        public bool testeXequemate(Cor cor)
+        {
+            if (!estaEmXeque(cor))
+            {
+                return false;
+            }
+
+            foreach (Peca item in pecasEmJogo(cor))
+            {
+                bool[,] mat = item.movimentosPossiveis();
+                for (int i = 0; i < tab.linhas; i++)
+                {
+                    for (int j = 0; j < tab.colunas; j++)
+                    {
+                        if (mat[i, j])
+                        {
+                            Posicao origem = item.posicao;
+                            Posicao destino = new Posicao(i, j);
+                            Peca pecaCapturada = executaMovimento(origem, destino);
+                            bool testeXeque = estaEmXeque(cor);
+                            desfazMovimento(origem, destino, pecaCapturada);
+                            if (!testeXeque)
+                            {
+                                return false;
+                            }
+                        }
+                    }
+                }
+            }
+            return true;
         }
 
         public void colocarNovaPeca(char coluna, int linha, Peca peca)
