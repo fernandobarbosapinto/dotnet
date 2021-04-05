5e297e98386956b15e04455b1155644cd4ed1423
Fernando Barbosa Pinto <f.barbosa@guide.com.br>
2021-04-05 09:16:46 -0300

    Aprendendo FileInfo método CopyTo

---
+++ trabalhando-com-arquivos/CourseExampleFile/CourseExampleFile/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace CourseExampleFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcePath = @"c:\Temp\file1.txt";
+            string targetPath = @"c:\Temp\file2.txt";
+
+            try
+            {
+                FileInfo fileInfo = new FileInfo(sourcePath);
+                fileInfo.CopyTo(targetPath);
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error ocurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
