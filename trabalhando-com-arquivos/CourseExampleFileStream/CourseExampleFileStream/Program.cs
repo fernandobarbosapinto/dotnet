23c9759d8f90c4dbdb90dca8528ebc49ff1b4df9
Fernando Barbosa Pinto <f.barbosa@guide.com.br>
2021-04-05 13:26:46 -0300

    Utilizando o File.OpenText ao invés do FileStream

--- trabalhando-com-arquivos/CourseExampleFileStream/CourseExampleFileStream/Program.cs
+++ trabalhando-com-arquivos/CourseExampleFileStream/CourseExampleFileStream/Program.cs
@@ -8,13 +8,11 @@ namespace CourseExampleFileStream
         static void Main(string[] args)
         {
             string path = @"c:\temp\file1.txt";
-            FileStream fs = null;
             StreamReader sr = null;
 
             try
             {
-                fs = new FileStream(path, FileMode.Open);
-                sr = new StreamReader(fs);
+                sr = File.OpenText(path);
                 string line = sr.ReadLine();
                 Console.WriteLine(line);
             }
@@ -26,7 +24,6 @@ namespace CourseExampleFileStream
             finally
             {
                 if (sr != null) sr.Close();
-                if (fs != null) fs.Close();
             }
         }
     }
