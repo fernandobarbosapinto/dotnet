c53fb3673c972a76d347707463c2c0f703c04ad4
Fernando Barbosa Pinto <f.barbosa@guide.com.br>
2021-04-05 13:39:15 -0300

    Utilizando o bloco Using

---
+++ trabalhando-com-arquivos/CourseExampleBlocoUsing/CourseExampleBlocoUsing/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace CourseExampleBlocoUsing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"c:\temp\file1.txt";
+
+            using (FileStream fs = new FileStream(path, FileMode.Open))
+            {
+                using (StreamReader sr = new StreamReader(fs))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string line = sr.ReadLine();
+                        Console.WriteLine(line);
+                    }
+                }
+            }
+        }
+    }
+}
