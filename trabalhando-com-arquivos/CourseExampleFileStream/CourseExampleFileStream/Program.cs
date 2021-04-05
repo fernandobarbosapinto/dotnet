d681618c7990e37e2bf2ef4d07ff579c0e6ec7f7
Fernando Barbosa Pinto <f.barbosa@guide.com.br>
2021-04-05 11:57:36 -0300

    Acessando arquivos utilizando FileStream e StreamReader

---
+++ trabalhando-com-arquivos/CourseExampleFileStream/CourseExampleFileStream/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace CourseExampleFileStream
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"c:\temp\file1.txt";
+            FileStream fs = null;
+            StreamReader sr = null;
+
+            try
+            {
+                fs = new FileStream(path, FileMode.Open);
+                sr = new StreamReader(fs);
+                string line = sr.ReadLine();
+                Console.WriteLine(line);
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error ocurred");
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                if (sr != null) sr.Close();
+                if (fs != null) fs.Close();
+            }
+        }
+    }
+}
