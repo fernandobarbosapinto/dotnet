37d6a4132e839c6a1d648957e0818fbd35857631
Fernando Barbosa Pinto <f.barbosa@guide.com.br>
2021-04-13 10:20:34 -0300

    Utilizando o comando Directory para listar pastas

---
+++ trabalhando-com-arquivos/CourseExampleDirectory/CourseExampleDirectory/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace CourseExampleDirectory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"c:\temp\myfolder";
+
+            try
+            {
+                IEnumerable<string> folders = Directory.EnumerateDirectories(path, "*.*", SearchOption.AllDirectories);
+                Console.WriteLine("FOLDERS: ");
+                foreach (string folder in folders)
+                {
+                    Console.WriteLine(folder);
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error ocurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
