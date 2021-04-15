87cc59788c8979e4f2d5496703269ecd116c6c73
Fernando Barbosa Pinto <f.barbosa@guide.com.br>
2021-04-15 12:12:23 -0300

    Implementando solução sem interface

---
+++ Interfaces/CourseExampleInterface/CourseExampleInterface/Entities/Invoice.cs
@@ -0,0 +1,30 @@
+﻿using System.Globalization;
+
+namespace CourseExampleInterface.Entities
+{
+    class Invoice
+    {
+        public double BasicPayment { get; set; }
+        public double Tax { get; set; }
+
+        public Invoice(double basicPayment, double tax)
+        {
+            BasicPayment = basicPayment;
+            Tax = tax;
+        }
+
+        public double totalPayment {
+            get { return BasicPayment + Tax; }
+        }
+
+        public override string ToString()
+        {
+            return "Basic payment: "
+                + BasicPayment.ToString("F2", CultureInfo.InvariantCulture)
+                + "\nTax: "
+                + Tax.ToString("F2", CultureInfo.InvariantCulture)
+                + "\nTotal payment: "
+                + totalPayment.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Interfaces/CourseExampleInterface/CourseExampleInterface/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Globalization;
+using CourseExampleInterface.Entities;
+using CourseExampleInterface.Services;
+
+namespace CourseExampleInterface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter rental data");
+            Console.Write("Car Model: ");
+            string model = Console.ReadLine();
+
+            Console.Write("Pickup (dd/MM/yyyy hh:mm): ");
+            DateTime start = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+
+            Console.Write("Return (dd/MM/yyyy hh:mm): ");
+            DateTime finish = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+
+            Console.Write("Enter price per Hour:");
+            double hour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Enter price per Day:");
+            double day = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            CarRental carRental = new CarRental(start, finish, new Vehicle(model));
+
+            RentalServices rentalService = new RentalServices(hour, day);
+
+            rentalService.ProcessInvoice(carRental);
+
+            Console.WriteLine("INVOICE:");
+            Console.WriteLine(carRental.Invoice);
+        }
+    }
+}
