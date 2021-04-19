6f4c3888cfab92e800d3461a53db3ad7cdbac04f
Fernando Barbosa Pinto <f.barbosa@guide.com.br>
2021-04-19 16:17:03 -0300

    Exemplo simples de conjuntos

---
+++ Generics/CourseExampleConjuntos/CourseExampleConjuntos/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CourseExampleConjuntos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> set = new HashSet<string>();
+
+            set.Add("TV");
+            set.Add("Notebook");
+            set.Add("Tablet");
+
+            Console.WriteLine(set.Contains("TV"));
+
+            foreach (var item in set)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
