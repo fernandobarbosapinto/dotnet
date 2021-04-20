d91b71d17658749da4f184f29a5b5fd726a9f6d5
Fernando Barbosa Pinto <f.barbosa@guide.com.br>
2021-04-20 11:54:40 -0300

    Aprendendo comparação de igualdade utilizando conjuntos

--- Generics/CourseExampleConjuntos/CourseExampleConjuntos/Program.cs
+++ Generics/CourseExampleConjuntos/CourseExampleConjuntos/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using Course.Entities;
 
 namespace CourseExampleConjuntos
 {
@@ -7,6 +8,23 @@ namespace CourseExampleConjuntos
     {
         static void Main(string[] args)
         {
+            HashSet<Product> a = new HashSet<Product>();
+            a.Add(new Product("TV", 900.00));
+            a.Add(new Product("NOTEBOOK", 1200.00));
+
+            HashSet<Point> b = new HashSet<Point>();
+            b.Add(new Point(3, 4));
+            b.Add(new Point(5, 10));
+
+            Product prod = new Product("NOTEBOOK", 1200.00);
+            Console.WriteLine(a.Contains(prod));
+
+            Point p = new Point(5, 10);
+            Console.WriteLine(b.Contains(p));
+
+            //Quando o tipo é struct ele faz a comparação por conteúdo e não por referencia de memória.
+
+            /*
             HashSet<string> set = new HashSet<string>();
 
             set.Add("TV");
@@ -19,6 +37,7 @@ namespace CourseExampleConjuntos
             {
                 Console.WriteLine(item);
             }
+            */
         }
     }
 }
