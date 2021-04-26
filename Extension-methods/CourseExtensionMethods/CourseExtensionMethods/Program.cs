6275348956494aa96214ccde2585d38513e763c9
Fernando Barbosa Pinto <f.barbosa@guide.com.br>
2021-04-26 11:19:06 -0300

    Aprendendo Extensions Methods

---
+++ Extension-methods/CourseExtensionMethods/CourseExtensionMethods/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Course.Extensions;
+
+namespace CourseExtensionMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dt = new DateTime(2021, 04, 20, 8, 10, 45);
+            Console.WriteLine(dt.ElapsedTime());
+
+            string txt = "Good morning dear students";
+            Console.WriteLine(txt.Cut(10));
+        }
+    }
+}
