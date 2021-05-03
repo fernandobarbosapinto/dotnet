760420603950f0d5e61facec7e03c3b2e1eab975
Fernando Barbosa Pinto <f.barbosa@guide.com.br>
2021-05-03 15:45:06 -0300

    Ordenação da lista utilizando uma função lambda

--- Programacao-Funcional-Delegate-linQ/CourseExampleComparison/CourseExampleComparison/Program.cs
+++ Programacao-Funcional-Delegate-linQ/CourseExampleComparison/CourseExampleComparison/Program.cs
@@ -14,7 +14,7 @@ namespace CourseExampleComparison
             list.Add(new Product("Notebook", 1200.00));
             list.Add(new Product("Tablet", 450.00));
 
-            Comparison<Product> comp = CompareProducts;
+            Comparison<Product> comp = (p1, p2) => p1.Name.ToUpper().CompareTo(p2.Name.ToUpper());
 
             list.Sort(comp);
 
@@ -23,11 +23,6 @@ namespace CourseExampleComparison
                 Console.WriteLine(item);
             }
 
-        }
-
-        static int CompareProducts(Product p1, Product p2)
-        {
-            return p1.Name.ToUpper().CompareTo(p2.Name.ToUpper());
         }
     }
 }
